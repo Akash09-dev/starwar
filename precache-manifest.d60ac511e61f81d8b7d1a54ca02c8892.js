6648ebd3d0083551219392e4822611b8c0999f91
Akash Kumar Sharma <Akash.Sharma@rsystems.com>
2019-11-22 11:15:31 +0530

    Updates

---
+++ precache-manifest.d60ac511e61f81d8b7d1a54ca02c8892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082c3dee5301d352bad8bb15fa8427a3",
+    "url": "/starwar/index.html"
+  },
+  {
+    "revision": "784d5615eb2480007a0a",
+    "url": "/starwar/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b499390c57ff4387d75b",
+    "url": "/starwar/static/css/main.e05364bc.chunk.css"
+  },
+  {
+    "revision": "784d5615eb2480007a0a",
+    "url": "/starwar/static/js/2.8692e7e8.chunk.js"
+  },
+  {
+    "revision": "b499390c57ff4387d75b",
+    "url": "/starwar/static/js/main.2bf2886b.chunk.js"
+  },
+  {
+    "revision": "c1c1ee35bb7d7ae9ad9f",
+    "url": "/starwar/static/js/runtime-main.1147c6e8.js"
+  }
+]);
